822eabf2ca483a918300715611b064f54466f828
Ninel-Zabaykina <zabaykinanm@mer.ci.nsu.ru>
2022-05-18 00:20:33 +0700

    fixed buttons

--- views/site/index.php
+++ views/site/index.php
@@ -3,8 +3,10 @@
 /** @var yii\web\View $this */
 
 use app\assets\AppAsset;
+use yii\helpers\Html;
 use yii\helpers\Url;
 use yii\widgets\ActiveForm;
+use yii\widgets\Pjax;
 
 $this->title = 'GuitarToTab';
 
@@ -14,16 +16,20 @@ AppAsset::register($this);
 <?php if (Yii::$app->user->isGuest) { ?>
 <div class="hellotext">
     <h1 style="text-align: center">
-    <br>We are pleased to welcome you to the site GuitarToTab. </br>
+    <br>We are glad to welcome you to the site GuitarToTab. </br>
     Our service will help you quickly and easily get the tablature of any audio recording.
         In order to use our application you just need to register or log in if you already have a network account.
     </h1>
     <div style="text-align: center">
-        <button type="button" class="btn btn-secondary btn-lg" style="width: 250px; margin: 15px; ">Registration</button>
+        <?php Pjax::begin(); ?>
+        <?= Html::a("Registration", ['user/create'], ['class' => 'btn btn-secondary btn-lg', 'style'=>'width: 250px; margin: 15px;']) ?>
+        <?php Pjax::end(); ?>
     </div>
     <div> </div>
     <div style="text-align: center">
-        <button type="button" class="btn btn-secondary btn-lg" style="width: 250px;">Authorization</button>
+        <?php Pjax::begin(); ?>
+        <?= Html::a("Authorization", ['site/login'], ['class' => 'btn btn-secondary btn-lg', 'style'=>'width: 250px; margin: 15px;']) ?>
+        <?php Pjax::end(); ?>
     </div>
 </div>
 <?php } ?>
